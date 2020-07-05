b9d5f6dced254347628f7ec86d68ab2a35432009
Shin Maung <ethereal97@gmail.com>
2020-07-05 18:13:24 +0630

    symlink at / from /usr
    /bin -> /usr/bin
    /etc -> /usr/etc
    /lib -> /usr/lib
    /opt -> /usr/opt
    /tmp -> /usr/tmp
    /var -> /usr/var
    
    Added: /usr/var/service (DIR)
    Addedv /usr/var/log (DIR)

--- boot.sh
+++ boot.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-CURRENT_DIR="$(dirname ${BASH_SOURCE[0]})"
+ROOT="$(cd "$(dirname ${BASH_SOURCE[0]})" && echo $(pwd))"
+
+PATH="$"
+HOME=$ROOT/home
+TMPDIR=$ROOT/tmp
+PREFIX=$ROOT/usr
+SVDIR=$PREFIX/var/service
+LOGDIR=$PREFIX/var/log
+
+
 
